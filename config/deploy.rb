fe282423763f0c1ffd17717d5201ee23899c543b
Chia-Hung Lin <qaz741012@gmail.com>
2019-02-13 11:27:32 +0800

    Fix - bundle version

--- config/deploy.rb
+++ config/deploy.rb
@@ -43,6 +43,7 @@ set :passenger_restart_with_touch, true
 
 # set :rbenv_type, :user
 # set :rbenv_ruby, '2.5.1'
-# set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
+# # set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
 # set :rbenv_map_bins, %w{rake gem bundle ruby rails}
 # set :rbenv_roles, :all # default value
+# set :rbenv_custom_path, "/home/qaz741012/.rbenv"
