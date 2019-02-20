a591efe35b1145d6fad9fd0757b95fb01f0a6897
Chia-Hung Lin <qaz741012@gmail.com>
2019-02-20 11:31:49 +0800

    Add - caculate coach play score before saving daily records

--- app/models/personal_record.rb
+++ app/models/personal_record.rb
@@ -1,4 +1,22 @@
 class PersonalRecord < ApplicationRecord
+  before_create :count_coach_play_score
+
   belongs_to :player
   belongs_to :game
+
+  private
+
+  def count_coach_play_score
+    self.coach_play_score = self.pts * 2 +
+                            self.oreb * 4 +
+                            self.dreb * 3 +
+                            self.ast * 4 +
+                            self.blk * 6 +
+                            self.stl * 6 +
+                            self.tpm * 2 +
+                            (self.fga - self.fgm) * -1 +
+                            (self.fta - self.ftm) * -2 +
+                            self.tov * -4 +
+                            self.pf * -2
+  end
 end
