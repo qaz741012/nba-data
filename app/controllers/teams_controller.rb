a6fa5c1ef2070aa831bd6c5de938f36b0e3fd8cc
Chia-Hung Lin <qaz741012@gmail.com>
2019-02-27 14:00:49 +0800

    Add - use ajax to team index page, and improve performance by reducing rendering partial template

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,9 +1,11 @@
 class TeamsController < ApplicationController
+  respond_to :html, :js, only: :index
+
   def index
     @subareas = Subarea.all
-    @subarea_id = params[:subarea_id]
+    @subarea_id = params[:subarea_id] || "all"
 
-    if @subarea_id
+    if @subarea_id != "all"
       @teams = Team.where(subarea_id: @subarea_id)
     else
       @teams = Team.all
